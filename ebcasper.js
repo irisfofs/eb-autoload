3569d7fe7110cee85cceae80e001257ab418949e
Sheva Goldberg <shevadas@gmail.com>
2016-06-21 13:45:27 -0500

    2016 updates and cleanup

--- ebcasper.js
+++ ebcasper.js
@@ -2,11 +2,15 @@
 
 var casper = require('casper').create({
 	pageSettings: {
-		loadImages:  false,        // The WebPage instance used by Casper will
-		loadPlugins: false         // use these settings
+		loadImages: false,	// The WebPage instance used by Casper will
+		loadPlugins: false	// use these settings
 	},
-	logLevel: "debug",              // Only "info" level messages will be logged
-	verbose: true                  // log messages will be printed out to the console
+	viewportSize: {
+		width: 1600,
+		height: 950
+	},
+	logLevel: "debug", // All messages will be logged
+	verbose: true      // log messages will be printed out to the console
 });
 
 var data = require(casper.cli.args[0]);
@@ -14,97 +18,81 @@ var badgeData = data["badges"];
 var badgeTypeString = data["badgeTypeString"];
 var badgePaymentType = data["badgePaymentType"];
 
-
 var config = require("config.json")
-// Shift off the header row. That's not helpful.
-// var headers = badgeData.shift();
-
-// API Test: 1234567890
-// Real Event 1234567890
 var eid = config["eventId"];
 
 var COUNT = -1;
 var FULL_NAME = 0;
 var BADGE_NAME = 1;
 var EMAIL = 2;
-var FIRST_BADGE_COLUMN = 0;
-var LAST_BADGE_COLUMN = 15;
-
-// var badgeData = [/*[1,"Lorem Ipsum","DevBadgerDontAccept","registration@bronycon.org",""],*/
-                 // [1,"John Testerman","Test","registration@bronycon.org",""]];
 
 casper.start("https://www.eventbrite.com/login/", function() {
-	// Set it up so remote logging gets replayed locally
-	// casper.on("remote.message", function(message) {
-	// 	this.echo("remote console.log: " + message);
-	// });
-
 	casper.on( 'page.error', function (msg, trace) {
 		this.echo( 'Error: ' + msg, 'ERROR' );
 	});
 
-  var loginCredentials = {
-    email: config["login"]["email"],
-    password: config["login"]["password"]
-  };
+	var loginCredentials = {
+		email: config["login"]["email"],
+		password: config["login"]["password"]
+	};
 
-	this.fill("form.responsive-form.l-block-3", loginCredentials, false);
-  casper.click("form.responsive-form.l-block-3 input[type='submit']");
+	this.fill('div#authentication-container form', loginCredentials, false);
+	casper.click('input[value="Log in"]');
 
-  capture(casper, "at_login.png");
+	capture(casper, "at_login.png");
 });
 
 casper.wait(10000, function() {
-  // Need to wait for some amount of time for the login to finish
-})
+	// Need to wait for some amount of time for the login to finish
+});
 
 casper.waitForUrl("https://www.eventbrite.com/", function() {
-  capture(casper, "post_login.png");
+	capture(casper, "post_login.png");
 
 	var iterationCount = 0;
 	casper.each(badgeData, function(self, badgeInfo) {
 
-    if(COUNT < 0) {
-      var countOfBadges = 1;
-    } else {
-      var countOfBadges = badgeInfo[COUNT];
-    }
+		if(COUNT < 0) {
+			var countOfBadges = 1;
+		} else {
+			var countOfBadges = badgeInfo[COUNT];
+		}
 
 		self.thenOpen('https://www.eventbrite.com/attendees-add?eid='+eid, function() {
 			this.page.injectJs('includes/jquery.min.js');
 
-      capture(self, 'attendees_add_pre.png');
-				
-			var name = this.evaluate(function(badgeTypeString) {
-        // console.log($("tr.ticket_row td:first-child").length);
-				var temp = $("tr.ticket_row td:first-child").filter(function() {
-					var txt = $(this).find("div").text();
-          // Do exact comparison, otherwise can't pick badges that are strict 
-          // substrings of earlier ones
-          return badgeTypeString === txt;
-					// console.log("(" + badgeTypeString + ") Badge type " + txt + " || " + (badgeTypeString === txt));
-          // var index = txt.indexOf(badgeTypeString);
-					// return index > -1; // JavaScript has no 'contains' method, apparently
-				});
-        // console.log(temp.length);
-				return temp.parent().find('input[name^="quant"]')[0].name;
-			}, badgeTypeString);
-			this.echo("Input name " + name, 'INFO');
-			
-			// Have to create object this way to set a property name based on a variable
-			var obj = {};
-			obj['input[name="'+name+'"]'] = countOfBadges;
-			this.fillSelectors('form[name="ticketForm"]', obj, false);
+			capture(self, 'attendees_add_pre.png');
 
-			// Change the payment type dropdown. Should be PayPal for vendors and complimentary for everyone else.
-			this.evaluate(function(badgePaymentType) {
-				$("#pp_payment_status").val(badgePaymentType).change();
-			}, badgePaymentType);
+			self.waitForSelector('form[name="ticketForm"]', function () {
+				var badgeQuantityInputName = this.evaluate(function(badgeTypeString) {
+					var temp = $("tr.ticket_row td:first-child").filter(function() {
+						var txt = $(this).find("div").text();
+						// Do exact comparison, otherwise can't pick badges that are strict
+						// substrings of earlier ones
+						return badgeTypeString === txt;
+					});
 
-			capture(self, 'attendees-add.png');
+					return temp.parent().find('input[id^="quant"]')[0].name;
+				}, badgeTypeString);
 
-			// Submit the form
-			this.fill('form[name="ticketForm"]', {}, true);
+				this.echo("Input name " + badgeQuantityInputName, 'DEBUG');
+
+				// Have to create object this way to set a property name based on a variable
+				var obj = {};
+				obj['input[name="'+badgeQuantityInputName+'"]'] = countOfBadges;
+				this.fillSelectors('form[name="ticketForm"]', obj, false);
+
+				// Change the payment type dropdown.
+				// Should be 'paypal' for vendors and 'comp' for everyone else.
+				this.evaluate(function(badgePaymentType) {
+					$("#pp_payment_status").val(badgePaymentType).change();
+				}, badgePaymentType);
+
+				capture(self, 'attendees-add.png');
+
+				// Submit the form
+				this.fill('form[name="ticketForm"]', {}, true);
+			});
 		});
 
 		// Wait for the page to load.
@@ -112,6 +100,7 @@ casper.waitForUrl("https://www.eventbrite.com/", function() {
 			this.page.injectJs('includes/jquery.min.js');
 
 			// ********************* Order information
+			// Split full name into first and last by the first space
 			var fullNameArr = badgeInfo[FULL_NAME].trim().split(' ');
 			var fullNameFirst = fullNameArr.shift();
 			var fullNameLast = fullNameArr.join(' ');
@@ -132,10 +121,10 @@ casper.waitForUrl("https://www.eventbrite.com/", function() {
 			var attendeeEmailAddresses = this.getElementsAttribute('input[name$="_email_address"]:not([name*="confirm"])', 'name');
 			// Custom questions
 			var attendeeBadgeNames = extractCustomQuestionNames(this, "Badge Name");
+			var attendeeWaivers = extractCustomQuestionNames(this, "Waivers");
 
 			// Make it once so we can fill everything at once >:D
 			var selectorObj = {};
-			// Split full name into first and last by the first space
 
 			// If the badge name is empty, use the "first" name
 			var badgeName = badgeInfo[BADGE_NAME].trim();
@@ -148,6 +137,7 @@ casper.waitForUrl("https://www.eventbrite.com/", function() {
 			selectorObj['input[name='+attendeeLastNames[0]+']'] = fullNameLast;
 			selectorObj['input[name='+attendeeEmailAddresses[0]+']'] = trimmedEmail; // always the same
 			selectorObj['input[name='+attendeeBadgeNames[0]+']'] = badgeName;
+			selectorObj['input[name='+attendeeWaivers[0]+']'] = true; // check waiver box
 
 			this.fillSelectors('form#registrationForm', selectorObj, false);
 
@@ -157,7 +147,6 @@ casper.waitForUrl("https://www.eventbrite.com/", function() {
 			this.fill('form#registrationForm', {}, true);
 		}, function(){ this.echo("Timed out waiting for form to load on row " + iterationCount + ": " + badgeInfo, 'ERROR'); }, 20000);
 		//	I think it has to submit something to finalize it.
-		// https://www.eventbrite.com/reports
 		self.waitForUrl(/^https?:\/\/www\.eventbrite\.com\/reports.*/, function() {
 			iterationCount++; // Increase our iteration! // Not sure why I'm doing this!
 		}, function(){ this.echo("Timed out trying to add row " + iterationCount + ": " + badgeInfo, 'ERROR'); }, 20000);
@@ -166,10 +155,14 @@ casper.waitForUrl("https://www.eventbrite.com/", function() {
 
 function extractCustomQuestionNames(casperInstance, question) {
 	return casperInstance.evaluate(function(questionText) {
-		var temp = $("tr.survey_question td:first-child").filter(function() {
+		// return all td elements matching the given question text
+		var temp = $("tr td:first-child").filter(function() {
 			return $(this).text().indexOf(questionText) > -1;
 		});
-		var domNodes = temp.parent().find('input').get();
+		// the actual input is in the next table row, so get the td's parent tr
+		// and then get the next tr over, because Eventbrite sucks
+		var domNodes = temp.parent().next().find('input').get();
+		// return array containing the names of all specified custom question inputs
 		return [].map.call(domNodes, function(node) {
 			return node.getAttribute('name');
 		});
